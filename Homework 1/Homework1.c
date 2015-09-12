ed922efb5b4c1428dd3ea6cb0c2e0452b32898de
caday00 <chaday00@gmail.com>
2015-09-11 21:52:55 -0400

    Update to homeWork1.c
    I've made a little progress on the program. Right now we can accept user input (even formatted). 
    
    Still working: When trying to extract the data in the 2nd for loop; only the last entered string is printed...any ideas?

--- Homework 1/Homework1.c
+++ Homework 1/Homework1.c
@@ -1,49 +1,26 @@
-﻿#include<stdio.h>
-#include<strings.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-
-DEFINE upperBound = 100;
-DEFINE lowerBound = 1;
-
-
-int main (parse arguments for user decisions for ascending/descending)
+int main (void)
 {
-error check- validate user input includes only one choice for ascending/descending)
- 
- printf("Enter 10 strings from 1 to 10 characters.\n");
+ printf("Enter 10 strings from 1 to 100 characters.\n");
 
-
- int lcv;
- char str[10]; //start off with small array
+ char *storage[11];
+ int i; 
+ char input[100];
  
- for (lcv = 0; lcv <= 3; lcv++)
+ for (i = 0; i <= 9; i++)
  {
-  printf("Enter a string: "); /*ask for 10 separate strings*/
-  scanf("%s", str); //send to compare() return 1 if not in list; 0 if it is
- 
- error check: if (str is between 1 and 100 AND is not a SPACE)
-  
+  printf("Enter a string: ");
+  fgets(input, sizeof(input),stdin);
+  input[strlen(input)-1] = '\0';
+  storage[i] = input;
  }
- while (str)
+
+ for (i = 0; i <= 9; i++)
  {
-  printf("You entered: %s\n", str);
+  printf("You entered: %s\n", storage[i]);
  }
  return 0;
 }
-
-
-
-
-MakeFile
-CFLAGS=-Wall -g
-
-clean:
-  rm -f Homekwork1
-
-
-int Compare (char [])
-{}
-
-
-int sort(void)
-{}
