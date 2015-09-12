674addf557a586814f77b535e1acc3ed29b4c18c
caday00 <chaday00@gmail.com>
2015-09-11 22:35:06 -0400

    Update to homeWork1.c
    Updated the main method; can now enter 10 strings up to 100 chars long including user formatting and prints out what they printed.

--- Homework 1/Homework1.c
+++ Homework 1/Homework1.c
@@ -1,26 +1,31 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
 
-int main (void)
+int main (int argc, char *argv[])
 {
  printf("Enter 10 strings from 1 to 100 characters.\n");
 
- char *storage[11];
- int i; 
- char input[100];
+ char input[11][102]; //double array to store 10 strings with max length of 100 characters
+ //unsure why we needed 102 here to store 100 characters...any ideas?
  
- for (i = 0; i <= 9; i++)
+ int i; 
+ //loop control variable; can be initialized in the for loop with "gcc -std=c99 homeWork1.c -o homeWork1"
+ //i.e., for (int i =0; i <= 9; i++); otherwise user format below
+ for (i = 0; i <= 9; i++) //only ask for 10 strings
  {
   printf("Enter a string: ");
-  fgets(input, sizeof(input),stdin);
-  input[strlen(input)-1] = '\0';
-  storage[i] = input;
+  fgets(input[i], sizeof(input[i]),stdin); 
+  /*
+   Capture user input and save it to each element in the array
+   Using fgets() allows us to capture user formatted data and test the size of the input;
+   size is based on the value of the array, in this case 102
+  */
  }
 
  for (i = 0; i <= 9; i++)
  {
-  printf("You entered: %s\n", storage[i]);
+  //print out each of the elements in the double array
+  printf("You entered: %s\n", input[i]);
  }
  return 0;
 }
